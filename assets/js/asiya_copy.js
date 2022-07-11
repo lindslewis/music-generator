3b7c33fa26807e581de01bfa5aee4864b92fc503
LindsayLew <lmschwehr@gmail.com>
2022-07-11 00:10:55 -0700

    kind of got the player looking right...

--- assets/js/asiya_copy.js
+++ assets/js/asiya_copy.js
@@ -1,4 +1,15 @@
-const musicContainer = document.getElementById('music-container');
+// const musicContainer = document.getElementById('music-container');
+// const playBtn = document.getElementById('play');
+// const prevBtn = document.getElementById('prev');
+// const nextBtn = document.getElementById('next');
+
+// const audio = document.getElementById('audio');
+// const progress = document.getElementById('progress');
+// const progressContainer = document.getElementById('progress-container');
+// const title = document.getElementById('title');
+// const cover = document.getElementById('cover');
+
+const musicContainer = document.getElementsByClassName('music-container');
 const playBtn = document.getElementById('play');
 const prevBtn = document.getElementById('prev');
 const nextBtn = document.getElementById('next');
@@ -8,6 +19,7 @@ const progress = document.getElementById('progress');
 const progressContainer = document.getElementById('progress-container');
 const title = document.getElementById('title');
 const cover = document.getElementById('cover');
+
 const currTime = document.querySelector('#currTime');
 const durTime = document.querySelector('#durTime');
 
