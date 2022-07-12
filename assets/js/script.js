f1c604f5dbe289ab70e1d1e71f70ad50450d34f8
Stephanie Gomez <stephaniemariagomez@gmail.com>
2022-07-12 15:47:48 -0700

    created HTML and click event for playlist info page

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,24 +7,25 @@ var btnL = document.querySelector("#OpenL")
 var btnS = document.querySelector("#OpenS")
 var discoBtn = document.querySelector("#disco")
 var discoInput = document.querySelector("#disco-search")
+var playlistBtn = document.querySelector("#playlist-page")
 
 
 
 
 //function to open a new page during concert search with local storage set
-conBtn.addEventListener("click", search) 
+conBtn.addEventListener("click", search)
 
 function search(event) {
     event.preventDefault()
+    if (searchInput.value > 0) {
+        localStorage.setItem("sTerm", searchInput.value);
 
-    localStorage.setItem("sTerm", searchInput.value);
 
-
-    window.open(
-        'HTML/results.html',
-        '_blank'    
-     )
-    
+        window.open(
+            'HTML/results.html',
+            '_blank'
+        )
+    }
 }
 
 //function to open a new page during discography search with local storage set
@@ -33,11 +34,19 @@ discoBtn.addEventListener("click", scanDisco)
 function scanDisco(event) {
     event.preventDefault()
     var searchTerm = discoInput.value
-    if (searchTerm.length>0){
-    localStorage.setItem("searchTerm", searchTerm);
-    window.open('HTML/disco.html', '_blank');
+    if (searchTerm.length > 0) {
+        localStorage.setItem("searchTerm", searchTerm);
+        window.open('HTML/disco.html', '_blank');
+    }
 }
-}
+
+//listener event to bring up playlist page
+playlistBtn.addEventListener("click", playListPage)
+
+    function playListPage(event) {
+        event.preventDefault()
+        window.open('HTML/playlists.html', '_blank');
+    }
 
 //option to like song (Listener event click box)
 //if I like a song (could be if/ else if statements)
